144c479dde665b554b9be884e4357c9e2a6b715b
CostelusE <62852858+CostelusE@users.noreply.github.com>
2020-05-13 12:04:37 +0300

    Add files via upload

---
+++ Proiect/Proiect/Control_Search.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Proiect
+{
+    partial class Control_Search
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtNume1 = new System.Windows.Forms.TextBox();
+            this.txtPrenume1 = new System.Windows.Forms.TextBox();
+            this.lblNume1 = new System.Windows.Forms.Label();
+            this.lblPrenume1 = new System.Windows.Forms.Label();
+            this.btnCauta = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbNumeComplet = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textNPrenume = new System.Windows.Forms.TextBox();
+            this.textNNume = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnModifica = new System.Windows.Forms.Button();
+            this.btnModificaPers = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.lblSuccesAdaugare = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Algerian", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.ForeColor = System.Drawing.Color.Black;
+            this.btnSearch.Location = new System.Drawing.Point(488, 142);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(177, 50);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNume1
+            // 
+            this.txtNume1.Font = new System.Drawing.Font("Elephant", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNume1.ForeColor = System.Drawing.Color.Black;
+            this.txtNume1.Location = new System.Drawing.Point(469, 250);
+            this.txtNume1.Name = "txtNume1";
+            this.txtNume1.Size = new System.Drawing.Size(222, 29);
+            this.txtNume1.TabIndex = 1;
+            // 
+            // txtPrenume1
+            // 
+            this.txtPrenume1.Font = new System.Drawing.Font("Elephant", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrenume1.ForeColor = System.Drawing.Color.Black;
+            this.txtPrenume1.Location = new System.Drawing.Point(469, 330);
+            this.txtPrenume1.Name = "txtPrenume1";
+            this.txtPrenume1.Size = new System.Drawing.Size(222, 29);
+            this.txtPrenume1.TabIndex = 2;
+            // 
+            // lblNume1
+            // 
+            this.lblNume1.AccessibleName = "lblNume";
+            this.lblNume1.AutoSize = true;
+            this.lblNume1.Font = new System.Drawing.Font("Elephant", 11F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNume1.ForeColor = System.Drawing.Color.Black;
+            this.lblNume1.Location = new System.Drawing.Point(464, 222);
+            this.lblNume1.Name = "lblNume1";
+            this.lblNume1.Size = new System.Drawing.Size(73, 25);
+            this.lblNume1.TabIndex = 3;
+            this.lblNume1.Text = "Name";
+            // 
+            // lblPrenume1
+            // 
+            this.lblPrenume1.AccessibleName = "lblPrenume";
+            this.lblPrenume1.AutoSize = true;
+            this.lblPrenume1.Font = new System.Drawing.Font("Elephant", 11F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrenume1.ForeColor = System.Drawing.Color.Black;
+            this.lblPrenume1.Location = new System.Drawing.Point(464, 302);
+            this.lblPrenume1.Name = "lblPrenume1";
+            this.lblPrenume1.Size = new System.Drawing.Size(128, 25);
+            this.lblPrenume1.TabIndex = 4;
+            this.lblPrenume1.Text = "First Name";
+            // 
+            // btnCauta
+            // 
+            this.btnCauta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnCauta.Font = new System.Drawing.Font("Elephant", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCauta.ForeColor = System.Drawing.Color.White;
+            this.btnCauta.Location = new System.Drawing.Point(515, 400);
+            this.btnCauta.Name = "btnCauta";
+            this.btnCauta.Size = new System.Drawing.Size(111, 35);
+            this.btnCauta.TabIndex = 5;
+            this.btnCauta.Text = "Search";
+            this.btnCauta.UseVisualStyleBackColor = false;
+            this.btnCauta.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(409, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(680, 663);
+            this.panel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DimGray;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(455, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 62);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "The person was\'n found";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // rtbNumeComplet
+            // 
+            this.rtbNumeComplet.BackColor = System.Drawing.Color.Gray;
+            this.rtbNumeComplet.Font = new System.Drawing.Font("Elephant", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbNumeComplet.ForeColor = System.Drawing.Color.Black;
+            this.rtbNumeComplet.Location = new System.Drawing.Point(45, 94);
+            this.rtbNumeComplet.Name = "rtbNumeComplet";
+            this.rtbNumeComplet.Size = new System.Drawing.Size(287, 474);
+            this.rtbNumeComplet.TabIndex = 9;
+            this.rtbNumeComplet.Text = "";
+            this.rtbNumeComplet.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button3.Font = new System.Drawing.Font("Elephant", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(123, 586);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 37);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Display";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(19, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(340, 60);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Display all the persons from the Agenda";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textNPrenume
+            // 
+            this.textNPrenume.Font = new System.Drawing.Font("Elephant", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNPrenume.ForeColor = System.Drawing.Color.Black;
+            this.textNPrenume.Location = new System.Drawing.Point(815, 330);
+            this.textNPrenume.Name = "textNPrenume";
+            this.textNPrenume.Size = new System.Drawing.Size(222, 29);
+            this.textNPrenume.TabIndex = 14;
+            // 
+            // textNNume
+            // 
+            this.textNNume.Font = new System.Drawing.Font("Elephant", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNNume.ForeColor = System.Drawing.Color.Black;
+            this.textNNume.Location = new System.Drawing.Point(815, 250);
+            this.textNNume.Name = "textNNume";
+            this.textNNume.Size = new System.Drawing.Size(222, 29);
+            this.textNNume.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AccessibleName = "lblNume";
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Elephant", 11F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(810, 222);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 25);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "New Name";
+            // 
+            // label5
+            // 
+            this.label5.AccessibleName = "lblNume";
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Elephant", 11F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(810, 302);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(179, 25);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "New First Name";
+            // 
+            // btnModifica
+            // 
+            this.btnModifica.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnModifica.Font = new System.Drawing.Font("Elephant", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModifica.ForeColor = System.Drawing.Color.White;
+            this.btnModifica.Location = new System.Drawing.Point(860, 400);
+            this.btnModifica.Name = "btnModifica";
+            this.btnModifica.Size = new System.Drawing.Size(111, 35);
+            this.btnModifica.TabIndex = 18;
+            this.btnModifica.Text = "Modify";
+            this.btnModifica.UseVisualStyleBackColor = false;
+            this.btnModifica.Click += new System.EventHandler(this.btnModify_Click);
+            // 
+            // btnModificaPers
+            // 
+            this.btnModificaPers.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnModificaPers.Font = new System.Drawing.Font("Cooper Black", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificaPers.ForeColor = System.Drawing.Color.White;
+            this.btnModificaPers.Location = new System.Drawing.Point(506, 492);
+            this.btnModificaPers.Name = "btnModificaPers";
+            this.btnModificaPers.Size = new System.Drawing.Size(483, 42);
+            this.btnModificaPers.TabIndex = 19;
+            this.btnModificaPers.Text = "Choose another person to change his name";
+            this.btnModificaPers.UseVisualStyleBackColor = false;
+            this.btnModificaPers.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnModify
+            // 
+            this.btnModify.Font = new System.Drawing.Font("Algerian", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModify.ForeColor = System.Drawing.Color.Black;
+            this.btnModify.Location = new System.Drawing.Point(835, 142);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(177, 50);
+            this.btnModify.TabIndex = 1;
+            this.btnModify.Text = "Modify";
+            this.btnModify.UseVisualStyleBackColor = true;
+            this.btnModify.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // lblSuccesAdaugare
+            // 
+            this.lblSuccesAdaugare.BackColor = System.Drawing.Color.DimGray;
+            this.lblSuccesAdaugare.Font = new System.Drawing.Font("Cooper Black", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSuccesAdaugare.ForeColor = System.Drawing.Color.White;
+            this.lblSuccesAdaugare.Location = new System.Drawing.Point(488, 260);
+            this.lblSuccesAdaugare.Name = "lblSuccesAdaugare";
+            this.lblSuccesAdaugare.Size = new System.Drawing.Size(535, 137);
+            this.lblSuccesAdaugare.TabIndex = 20;
+            this.lblSuccesAdaugare.Text = "Change made";
+            this.lblSuccesAdaugare.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Control_Search
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.rtbNumeComplet);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtNume1);
+            this.Controls.Add(this.btnCauta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPrenume1);
+            this.Controls.Add(this.txtPrenume1);
+            this.Controls.Add(this.lblNume1);
+            this.Controls.Add(this.btnModificaPers);
+            this.Controls.Add(this.lblSuccesAdaugare);
+            this.Controls.Add(this.btnModifica);
+            this.Controls.Add(this.textNPrenume);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textNNume);
+            this.Controls.Add(this.label6);
+            this.Name = "Control_Search";
+            this.Size = new System.Drawing.Size(1174, 679);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtNume1;
+        private System.Windows.Forms.TextBox txtPrenume1;
+        private System.Windows.Forms.Label lblNume1;
+        private System.Windows.Forms.Label lblPrenume1;
+        private System.Windows.Forms.Button btnCauta;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbNumeComplet;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textNPrenume;
+        private System.Windows.Forms.TextBox textNNume;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnModifica;
+        private System.Windows.Forms.Button btnModificaPers;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.Label lblSuccesAdaugare;
+    }
+}
---
+++ Proiect/Proiect/Control_Search.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Proiect
+{
+    public partial class Control_Search : UserControl
+    {
+        IStocareData adminPersoane = StocareFactory.GetAdministratorStocare();
+        public Control_Search()
+        {
+
+            
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnCauta.BringToFront();
+            lblNume1.BringToFront();
+            lblPrenume1.BringToFront();
+            txtPrenume1.BringToFront();
+            txtNume1.BringToFront();
+            label1.SendToBack();
+            lblSuccesAdaugare.SendToBack();
+            txtNume1.Enabled = true;
+            txtPrenume1.Enabled = true;
+            txtNume1.Clear();
+            txtPrenume1.Clear();
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Agenda s = adminPersoane.GetPersoana(txtNume1.Text, txtPrenume1.Text);
+            if (s != null)
+            {
+                
+                btnModifica.BringToFront();
+                label5.BringToFront();
+                label6.BringToFront();
+                textNNume.BringToFront();
+                textNPrenume.BringToFront();
+                txtNume1.Enabled = false;
+                txtPrenume1.Enabled = false;
+                btnModificaPers.BringToFront();
+            }
+            else
+            {
+                label1.BringToFront();
+                btnCauta.SendToBack();
+                lblNume1.SendToBack();
+                lblPrenume1.SendToBack();
+                txtPrenume1.SendToBack();
+                txtNume1.SendToBack();
+            }
+            
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            rtbNumeComplet.Clear();
+            ArrayList AGENDA = adminPersoane.GetPersoane();
+            foreach (Agenda pers in AGENDA)
+            {
+                rtbNumeComplet.AppendText(pers.Nume_Complet());
+                rtbNumeComplet.SelectionTabs = new int[] { 25 };
+                rtbNumeComplet.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+            if (textNNume.Text == "" || textNPrenume.Text == "")
+            {
+                if (textNNume.Text == "")
+                    label6.ForeColor = Color.Red;
+                else
+                    label6.ForeColor = Color.Black;
+                if (textNPrenume.Text == "")
+                    label5.ForeColor = Color.Red;
+                else
+                    label5.ForeColor = Color.Black;
+            }
+            else
+            {
+                Agenda s = adminPersoane.GetPersoana(txtNume1.Text, txtPrenume1.Text);
+                s.CautaSiModifica(textNNume.Text, textNPrenume.Text);
+                adminPersoane.UpdateStudent(s);
+                lblSuccesAdaugare.BringToFront();
+                btnModificaPers.SendToBack();
+                textNNume.SendToBack();
+                textNPrenume.SendToBack();
+                btnModifica.SendToBack();
+                btnCauta.SendToBack();
+                lblNume1.SendToBack();
+                lblPrenume1.SendToBack();
+                txtPrenume1.SendToBack();
+                txtNume1.SendToBack();
+                label6.SendToBack();
+                label5.SendToBack();
+                label5.ForeColor = Color.Black;
+                label6.ForeColor = Color.Black;
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            btnModificaPers.SendToBack();
+            txtNume1.Enabled = true;
+            txtPrenume1.Enabled = true;
+            btnModifica.SendToBack();
+            label5.SendToBack();
+            label6.SendToBack();
+            textNNume.SendToBack();
+            textNPrenume.SendToBack();
+            label5.ForeColor = Color.Black;
+            label6.ForeColor = Color.Black;
+
+        }
+    }
+}
